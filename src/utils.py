966153e34c7c612ef5f0b3e6ef152f9a7fe4ffe3
rui-2002 <sangwansumit628@gmail.com>
2025-07-29 19:27:27 +0530

    hyperparameter tunning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill # help to build pkl file
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 ## utils will have the common code required
 from src.exception import CustomException
 
@@ -20,14 +21,23 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=params[list(models.keys())[i]]  # Use keys, not values!
 
-            model.fit(X_train,y_train) #Train model
+            # hyperparameter tunning with param(parameters)
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+             
+            # selecting best parameters
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            # after hyperparamet training we don't need model.fit()
+            #model.fit(X_train,y_train) #Train model
 
             y_train_pred=model.predict(X_train)
 
